f4bb7034d27a6376062288e6ea160d262a35a358
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-06-09 18:01:49 +0600

    form setup

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         .form_body{
             display: flex;
             justify-content: center;
-            margin-top: 30px;
+            margin: 30px 0;
         }
         .form{
             display: flex;
@@ -38,9 +38,95 @@
                     <input type="text" name="nameSend" id="name" />
                 </span>
                 <span>
-                    <label for="email">email:</label>
+                    <label for="email">Email:</label>
                     <input type="email" name="emailSend" id="email" />
                 </span>
+                <span>
+                    <label for="phone">Phone:</label>
+                    <input type="tel" name="phone" id="phone" />
+                </span>
+                <span>
+                    <label for="age">Age:</label>
+                    <input type="number" name="age" id="age" />
+                </span>
+                <span>
+                    <label for="pass">Password:</label>
+                    <input type="password" name="pass" id="pass" />
+                </span>
+                <span>
+                    <label for="color">Favorite color:</label>
+                    <input type="color" name="color" id="color" />
+                </span>
+                <span>
+                    <label for="date">Birth date:</label>
+                    <input type="date" name="date" id="date" />
+                </span>
+                <span>
+                    <label for="app-date">Appiontment date:</label>
+                    <input type="datetime-local" name="appDate" id="app-date" />
+                </span>
+                <span>
+                    <label for="time">Launch Time:</label>
+                    <input type="time" name="time" id="time" />
+                </span>
+                <span>
+                    <p>Vehicles</p>
+                    <input type="checkbox" id="bike" name="bike" value="Bike">
+                    <label for="bike"> I have a bike</label><br>
+                    <input type="checkbox" id="car" name="car" value="Car">
+                    <label for="car"> I have a car</label><br>
+                    <input type="checkbox" id="boat" name="boat" value="Boat">
+                    <label for="boat"> I have a boat</label>
+                </span>
+                <span>
+                    <p>Please select your favorite Web language:</p>
+                      <input type="radio" id="html" name="fav_language" value="HTML">
+                      <label for="html">HTML</label><br>
+                      <input type="radio" id="css" name="fav_language" value="CSS">
+                      <label for="css">CSS</label><br>
+                      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+                      <label for="javascript">JavaScript</label>
+                </span>
+                <span>
+                    <label for="cars">Choose a car:</label>
+                    <select name="cars" id="cars">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </span>
+                <span>
+                    <p>Write your Review:</p>
+                    <textarea id="myReview" name="myReview" rows="4" cols="50"></textarea>
+                </span>
+                <span>
+                    <label for="week">Week:</label>
+                    <input type="week" name="weekInput" id="week">
+                </span>
+                <span>
+                    <label for="month">Month:</label>
+                    <input type="month" name="monthInput" id="month">
+                </span>
+                <span>
+                    <label for="url">Website Url:</label>
+                    <input type="url" name="urlInput" id="url">
+                </span>
+                <span>
+                    <label for="range">Your range:</label>
+                    <input type="range" name="rangeInput" id="range" min="0" max="100">
+                </span>
+                <span>
+                    <label for="image">Your Image:</label>
+                    <input type="image" name="imageInput" id="image">
+                </span>
+                <span>
+                    <label for="file">Your File:</label>
+                    <input type="file" name="fileInput" id="file">
+                </span>
+                <br/>
+                <br/>
+                <input type="button" value="Submit" name="submit">
             </form>
         </section>        
     </main>
